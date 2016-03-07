f317aad3e40322867d8f3d9cc3721995e7b96325
sekaranglila <sekaranglilah@gmail.com>
2016-03-07 22:37:44 +0700

    Update Tumbuhan.cpp

--- Makhluk/Tumbuhan/Tumbuhan.cpp
+++ Makhluk/Tumbuhan/Tumbuhan.cpp
@@ -26,3 +26,13 @@ int Tumbuhan::randomizeDir() {
 int Tumbuhan::randomizeT() {
     return (rand() % 3) + 3;
 }
+
+void Tumbuhan::setBattlePowerToOpponent(const Makhluk &M){
+    if ((M.getType() == "Herbivore") || (M.getType() == "Omnivore") || (M.getType() == "Men") || (M.getType() == "Female")){
+        setBattlePower(-1); //Always Lose
+    } else if (M.getType() == "Carnivore"){
+        setBattlePower(0); //Carnivores and Plants do not interact
+    } else {
+        setBattlePower(getPower()); //Normal Power
+    }
+}
